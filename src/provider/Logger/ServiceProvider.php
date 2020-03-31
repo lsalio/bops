1bd1d223c3d4ec328246d7ab6190224feec8e227
Jayson <shadowman@shellboot.com>
2020-04-01 00:13:52 +0800

    migrate to bops/logger

--- src/provider/Logger/ServiceProvider.php
+++ src/provider/Logger/ServiceProvider.php
@@ -8,10 +8,10 @@
  */
 namespace Bops\Provider\Logger;
 
+use Bops\Logger\Adapter\Stream;
+use Bops\Logger\Formatter\Adapter\Plain;
+use Bops\Logger\Level;
 use Bops\Provider\AbstractServiceProvider;
-use Phalcon\Logger;
-use Phalcon\Logger\Adapter\File as FileLogger;
-use Phalcon\Logger\Formatter\Line as LineFormatter;
 
 
 /**
@@ -37,20 +37,6 @@ class ServiceProvider extends AbstractServiceProvider {
      */
     public function register() {
         $this->di->set($this->name(), function(string $filename = '') {
-            $level = self::LOGGER_DEFAULT_LEVEL;
-            if ($config = env('SERVICE_LOGGER_LEVEL')) {
-                switch (strtolower($config)) {
-                    case 'emergency': $level = Logger::EMERGENCY; break;
-                    case 'critical': $level = Logger::CRITICAL; break;
-                    case 'alert': $level = Logger::ALERT; break;
-                    case 'error': $level = Logger::ERROR; break;
-                    case 'warning': $level = Logger::WARNING; break;
-                    case 'notice': $level = Logger::NOTICE; break;
-                    case 'info': $level = Logger::INFO; break;
-                    case 'debug': $level = Logger::DEBUG; break;
-                }
-            }
-
             if (empty($filename)) {
                 $filename = self::LOGGER_DEFAULT_FILENAME;
                 if ($config = env('SERVICE_LOGGER_FILENAME')) {
@@ -68,9 +54,9 @@ class ServiceProvider extends AbstractServiceProvider {
                 $dateFormat = $config;
             }
 
-            $logger = new FileLogger(container('navigator')->logDir("{$filename}.log"));
-            $logger->setFormatter(new LineFormatter($format, $dateFormat));
-            $logger->setLogLevel($level);
+            $logger = new Stream(container('navigator')->logDir("{$filename}.log"));
+            $logger->setFormatter(new Plain($format, $dateFormat));
+            $logger->setLevel(env('SERVICE_LOGGER_LEVEL', self::LOGGER_DEFAULT_LEVEL));
 
             return $logger;
         });
@@ -79,7 +65,7 @@ class ServiceProvider extends AbstractServiceProvider {
     /**
      * @const LOGGER_DEFAULT_LEVEL The default level of logger
      */
-    protected const LOGGER_DEFAULT_LEVEL = Logger::WARNING;
+    protected const LOGGER_DEFAULT_LEVEL = Level::INFO;
 
     /**
      * @const LOGGER_DEFAULT_FILENAME The name of the default logger
@@ -89,7 +75,7 @@ class ServiceProvider extends AbstractServiceProvider {
     /**
      * @const LOGGER_DEFAULT_FORMAT The format of the default logger
      */
-    protected const LOGGER_DEFAULT_FORMAT = '%date% - %type% \t: %message%';
+    protected const LOGGER_DEFAULT_FORMAT = '{date} - {type} \t: {message}';
 
     /**
      * @const LOGGER_DEFAULT_DATE_FORMAT The date format of the default logger
